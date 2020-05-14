c268c7d16df3715c678644bd4ac23ce8c8c8de2b
MykolaKlishch <mykola.klishch1707@gmail.com>
2020-05-14 14:06:04 +0300

    tictactoe.py: input processing put into move() function

--- tictactoe.py
+++ tictactoe.py
@@ -1,80 +1,77 @@
-"""Tic-tac-toe game simulator"""
+"""Tic-tac-toe game implementation
+
+Only two-player mode is available here
+"""
 
 import re
 
-VICTORY_COMBINATIONS = (
-    (0, 1, 2), (3, 4, 5), (6, 7, 8),  # horizontal lines
-    (0, 3, 6), (1, 4, 7), (2, 5, 8),  # vertical lines
-    (0, 4, 8), (2, 4, 6)              # diagonals
-)
-
-PLAYERS = ('o', 'x')                  # player marks
+PLAYERS = ('x', 'o')  # player marks
 
 
 def visualize(board):
-    """Prints the current state of game board
+    """Prints the current state of game board.
     :return: None
     """
     for i in range(3):
         if i == 0:
             print('┌───┬───┬───┐')
-        print(f'│ {board[i*3]} │ {board[i*3 + 1]} │ {board[i*3 + 2]} │')
+        print(f'│ {board[i * 3]} │ {board[i * 3 + 1]} │ {board[i * 3 + 2]} │')
         if i <= 1:
             print('├───┼───┼───┤')
         if i == 2:
             print('└───┴───┴───┘')
 
 
-def move_is_legit(board, position):
-    """Checks if the move is possible
+def get_pos(board, player):
+    """Processes input from a player.
+    Checks if input is valid and the move is possible.
+    If so, returns the position to be marked.
     :param board: list
-    :param position: int
-        from 1 to 9 only!
-        if the same integer value is present in board list
-        it means that this square is still empty
-    :return: boolean
+    :param player: str
+    :return: int
     """
-    return position in board
+    while True:
+        position = input(f'Player {player}: ')
+        if not re.match(r'^[1-9]$', position):  # incorrect input
+            print('Input should be a single digit '
+                  'from 1 to 9. Please try again.')
+            continue
+        position = int(position)
+        if position not in board:  # selected cell is not empty
+            print('Impossible move. Please try again.')
+            continue
+        return position
 
 
 def victory(board, player):
-    """Checks if the particular player has won the game
+    """Checks if the particular player has won the game.
     :param board: list
     :param player: str
         'x' or 'o'
     :return: boolean
     """
-    for L in VICTORY_COMBINATIONS:
+    for L in (  # indexes combinations for board list corresponding to:
+        (0, 1, 2), (3, 4, 5), (6, 7, 8),  # horizontal lines
+        (0, 3, 6), (1, 4, 7), (2, 5, 8),  # vertical lines
+        (0, 4, 8), (2, 4, 6)              # diagonals
+    ):
         if board[L[0]] == board[L[1]] == board[L[2]] == player:
             return True
     return False
 
 
-def _main():
+def game():
     board = [i + 1 for i in range(9)]
     visualize(board)
-    moves_left = 9
-    while moves_left > 0:
-        player = PLAYERS[moves_left % 2]
-        position = input(f'Player {player}: ')
-        if not re.match(r'^[1-9]$', position):
-            print('Input should be a single digit '
-                  'from 1 to 9. Please try again.')
-            continue
-        position = int(position)
-        if move_is_legit(board, position):
-            board[position-1] = player
-            visualize(board)
-            moves_left -= 1
-        else:
-            print('Impossible move. Please try again.')
-            continue
+    for move in range(9):
+        player = PLAYERS[move % 2]
+        board[get_pos(board, player) - 1] = player
+        visualize(board)
         if victory(board, player):
             print(f'Player {player} won!')
-            break
-        if moves_left == 0:
-            print('Draw!')
+            exit()
+    print('Draw!')
 
 
 if __name__ == "__main__":
-    _main()
+    game()
