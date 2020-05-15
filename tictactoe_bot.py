47ac24f1a92486c150bf48cbfcffef1bfc46e863
MykolaKlishch <mykola.klishch1707@gmail.com>
2020-05-15 17:50:29 +0300

    choose_pos_bot is now more compact

--- tictactoe_bot.py
+++ tictactoe_bot.py
@@ -71,6 +71,80 @@ def initialize_players():
         return human, bot
 
 
+def choose_pos_bot(board, player):
+    """Implements a strategy of a perfect tic-tac-toe player.
+
+    The strategy is implemented as a sequence of cases
+    that should be checked one after another
+    until the right case is found:
+
+    #1. Win: If the player has two in a row,
+    they can place a third to get three in a row.
+
+    #2. Block: If the opponent has two in a row,
+    the player must play the third themselves to block the opponent.
+
+    #3. Fork: Create an opportunity where the player
+    has two ways to win (two non-blocked lines of 2).
+
+    #4. Blocking an opponent's fork:
+    4.1. If there is only one possible fork for the opponent,
+         the player should block it.
+    4.2. Otherwise, the player should create a two in a row
+         to force the opponent into defending,
+         as long as it doesn't result in them creating a fork.
+         For example, if "x" has two opposite corners
+         and "O" has the center, "o" must not play a corner
+         in order to win. (Playing a corner in this scenario
+         creates a fork for "x" to win.)
+
+    #5. Center: A player marks the center.
+    (If it is the first move of the game, playing on a corner
+    gives the second player more opportunities to make a mistake
+    and may therefore be the better choice; however, it makes
+    no difference between perfect players.)
+
+    #6. Opposite corner: If the opponent is in the corner,
+    the player plays the opposite corner.
+
+    #7. Empty corner: The player plays in a corner square.
+    #8. Empty side: The player plays in a middle square
+    on any of the 4 sides.
+
+    :param board: list
+    :param player: str
+    :return: int
+    """
+    opponent = 'o' if player == 'x' else 'x'
+    true_forks = _select_true_forks(board, FORKS.keys())
+    params = {  # case: parameters set for _complete_combinations()
+        '1': (board, LINES, player),
+        '2': (board, LINES, opponent),
+        '3': (board, true_forks, player),
+        '4': (board, true_forks, opponent),
+        '6': (board, CORNERS, opponent)
+    }
+    for case in '12345678':
+        if case in params.keys():
+            comp_comb = _complete_combinations(*params[case])
+            if case == '4' and len(comp_comb) > 1:  # case 4.2
+                comp_comb = _complete_combinations(
+                    board, LINES, player, s=3, warning=comp_comb)
+            if comp_comb:
+                return list(comp_comb).pop(
+                    round(random.random() * len(comp_comb)) - 1)
+        elif case == '5':
+            if 5 in board and (player == 'o' or random.random() <= 0.2):
+                return 5  # player x will start from 5 in 20% of cases
+        elif case in '78':
+            corners, sides = [1, 3, 7, 9], [2, 4, 6, 8]
+            random.shuffle(corners)
+            random.shuffle(sides)
+            for pos in corners + sides + [5]:
+                if pos in board:
+                    return pos
+
+
 def _select_true_forks(board, forks):
     """A fork that is not open is not a fork
     :param board: list
@@ -137,81 +211,6 @@ def _complete_combinations(board, combinations,
                 marks = _handle_warning(marks, warning)
             comp_comb.update(marks - {'x', 'o'})
     return comp_comb
-
-
-def choose_pos_bot(board, player):
-    """Implements a strategy of a perfect tic-tac-toe player.
-
-    The strategy is implemented as a sequence of cases
-    that should be checked one after another
-    until the right case is found:
-
-    #1. Win: If the player has two in a row,
-    they can place a third to get three in a row.
-
-    #2. Block: If the opponent has two in a row,
-    the player must play the third themselves to block the opponent.
-
-    #3. Fork: Create an opportunity where the player
-    has two ways to win (two non-blocked lines of 2).
-
-    #4. Blocking an opponent's fork:
-    4.1. If there is only one possible fork for the opponent,
-         the player should block it.
-    4.2. Otherwise, the player should create a two in a row
-         to force the opponent into defending,
-         as long as it doesn't result in them creating a fork.
-         For example, if "x" has two opposite corners
-         and "O" has the center, "o" must not play a corner
-         in order to win. (Playing a corner in this scenario
-         creates a fork for "x" to win.)
-
-    #5. Center: A player marks the center.
-    (If it is the first move of the game, playing on a corner
-    gives the second player more opportunities to make a mistake
-    and may therefore be the better choice; however, it makes
-    no difference between perfect players.)
-
-    #6. Opposite corner: If the opponent is in the corner,
-    the player plays the opposite corner.
-
-    #7. Empty corner: The player plays in a corner square.
-    #8. Empty side: The player plays in a middle square
-    on any of the 4 sides.
-
-    :param board: list
-    :param player: str
-    :return: int
-    """
-
-    opponent = 'o' if player == 'x' else 'x'
-    true_forks = _select_true_forks(board, FORKS.keys())
-    params = {  # case: parameters set for _complete_combinations()
-        '1': (board, LINES, player),
-        '2': (board, LINES, opponent),
-        '3': (board, true_forks, player),
-        '4': (board, true_forks, opponent),
-        '6': (board, CORNERS, opponent)
-    }
-    for case in '12345678':
-        if case in params.keys():
-            comp_comb = _complete_combinations(*params[case])
-            if case == '4' and len(comp_comb) > 1:  # case 4.2
-                comp_comb = _complete_combinations(
-                    board, LINES, player, s=3, warning=comp_comb)
-            if comp_comb:
-                return list(comp_comb).pop(
-                    round(random.random() * len(comp_comb)) - 1)
-        elif case == '5':
-            if 5 in board and (player == 'o' or random.random() <= 0.2):
-                return 5  # player x will start from 5 in 20% of cases
-        elif case in '78':
-            corners, sides = [1, 3, 7, 9], [2, 4, 6, 8]
-            random.shuffle(corners)
-            random.shuffle(sides)
-            for pos in corners + sides + [5]:
-                if pos in board:
-                    return pos
 
 
 def _main():
