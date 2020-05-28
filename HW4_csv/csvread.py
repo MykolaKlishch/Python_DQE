759701c888d7efe3b909bf785c0c42278c02c48f
MykolaKlishch <mykola.klishch1707@gmail.com>
2020-05-28 22:33:13 +0300

    test commit

--- HW4_csv/csvread.py
+++ HW4_csv/csvread.py
@@ -12,5 +12,3 @@ def get_namespace(args=None):
                         help="number or HRR to display")
     parsed_args = parser.parse_args(args)
     return parsed_args.path, parsed_args.top
-#lglkjb
-#jn'lkm
