04eb4da8a3e34eedafe0b0bab3070855e5d9affd
MykolaKlishch <mykola.klishch1707@gmail.com>
2020-06-13 19:41:54 +0300

    sys.getsizeof() removed

--- HW6_xml/xmlparser_the_fastest.py
+++ HW6_xml/xmlparser_the_fastest.py
@@ -7,14 +7,6 @@ def get_government(country):
 
 def main():
     countries = et.parse("mondial-3.0.xml").getroot().iter("country")
-
-    import sys
-    print(sys.getsizeof(et.parse("mondial-3.0.xml")))
-    print(sys.getsizeof(et.parse("mondial-3.0.xml").getroot()))
-    print(sys.getsizeof(et.parse("mondial-3.0.xml").getroot().iter("country")))
-    print(sys.getsizeof(countries))
-    print(sys.getsizeof(set(map(get_government, countries))))
-
     governments = set(map(get_government, countries))
     print(*sorted(governments), sep=", ")
 
