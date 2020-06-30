34752cedd91d9c53e664591de3767510d8f9f2de
MykolaKlishch <mykola.klishch1707@gmail.com>
2020-06-30 12:05:10 +0300

    create_database_schema() is now a separate function

--- HW8_sqllite3/task_db_script.py
+++ HW8_sqllite3/task_db_script.py
@@ -2,11 +2,11 @@
 and provides the ability to execute queries on it.
 
 Detailed description:
- 1. Creates database as a single file.
- 2. Creates two tables in the database: projects_tbl, tasks_tbl.
+ 1. Creates sqlite3 database.
+ 2. Creates two tables in the database: projects_tbl and tasks_tbl.
     Defines column names, column data types, relations and constraints.
- 3. Imports data from the respective .csv files
-    and inserts these data into the tables.
+ 3. Imports data from .csv files and inserts these data into
+    the respective tables.
  4. Takes user input in a form of executable SQLite query.
     In order to extend functionality, user input requires a full query,
     not just a part of it. Queries on both tables using JOIN are
@@ -15,9 +15,10 @@ Detailed description:
  5. Prints selected data in a table. Automatically detects column width.
 
 File names, database and table names, column names, column data types,
-relations and constraints are hardcoded. File content can be modified as
-long as these modifications do not contradict with data types and
-constraints.
+relations and constraints are hardcoded because formatting sql queries
+is not safe due to potential SQL injections. File content can be
+modified as long as these modifications do not contradict with
+data types and constraints.
 """
 
 import csv
@@ -28,60 +29,57 @@ import sys
 from typing import Any, Iterable, NoReturn, Union
 
 
+def create_database_schema(cursor: sqlite3.Cursor) -> NoReturn:
+    """Executes SQL script which creates tables
+    and defines their structure.
+
+    :param cursor: sqlite3 cur object to execute statements.
+    """
+    cursor.executescript("""
+    DROP TABLE IF EXISTS projects_tbl;
+    CREATE TABLE projects_tbl(
+        project_id   NUMBER  PRIMARY KEY,
+        name         TEXT,
+        description  TEXT,
+        deadline     DATE
+    );
+    DROP TABLE IF EXISTS tasks_tbl;
+    CREATE TABLE tasks_tbl( 
+        task_id      NUMBER  PRIMARY KEY,
+        priority     INTEGER,
+        details      TEXT,
+        status       TEXT    CHECK(
+            status IN('new', 'pending', 'done', 'canceled')),
+        deadline     DATE,
+        completed    DATE,
+        project_id   NUMBER,
+        FOREIGN KEY(project_id) REFERENCES project_tbl(project_id)
+    );""")
+
+
 def import_data_into_database(cursor: sqlite3.Cursor) -> NoReturn:
     """Reads data from csv files and inserts them into tables.
     Can handle '\ufeff' character in csv files because it uses
     'utf-8-sig' encoding which ignores '\ufeff' ('utf-8' does not).
 
-    :param cursor: sqlite3 cursor object to execute statements.
+    :param cursor: sqlite3 cur object to execute statements.
     """
-    INSERTION_COMMANDS = {
+    insertion_commands = {
         "projects_tbl.csv": "INSERT INTO projects_tbl VALUES (?, ?, ?, ?);",
         "tasks_tbl.csv": "INSERT INTO tasks_tbl VALUES (?, ?, ?, ?, ?, ?, ?);"
     }
-    for filename in INSERTION_COMMANDS.keys():
+    for filename in insertion_commands.keys():
         if not os.path.exists(filename):
-            close_with_message_and_exit(cursor, f"Could not find '{filename}'!")
+            close_with_message_and_exit(cursor, f"Couldn't find '{filename}'!")
         reader = csv.reader(open(filename, encoding='utf-8-sig', newline=""))
         next(reader)
-        cursor.executemany(INSERTION_COMMANDS.get(filename), reader)
-
-
-def get_query(cursor: sqlite3.Cursor) -> NoReturn:
-    """Generator which gets queries from stdin.
-    Queries are NOT validated at this stage.
-
-    :param cursor: sqlite3 cursor object.
-    """
-    print("\nEnter your query below and press Enter twice to execute it:")
-    new_query = ""
-    while True:
-        new_query_line = next(sys.stdin)
-        if new_query_line.isspace():
-            break
-        new_query += new_query_line
-    if not new_query:
-        close_with_message_and_exit(cursor, "No query was detected")
-    yield new_query
-
-
-def close_with_message_and_exit(
-        cursor: sqlite3.Cursor,
-        message="") -> NoReturn:
-    """Prints message, closes the cursor and exits
-
-    :param cursor: sqlite3 cursor object to be closed
-    :param message: message to print before exit
-    """
-    print(message, end="")
-    cursor.close()
-    exit()
+        cursor.executemany(insertion_commands.get(filename), reader)
 
 
 def execute_user_queries(cursor: sqlite3.Cursor) -> NoReturn:
     """Get query from the user, execute it and print the response.
 
-        :param cursor: sqlite3 cursor object to execute queries.
+        :param cursor: sqlite3 cur object to execute queries.
     """
     print("""\nPlease enter SQL query and press double Enter.
     * Both inline and multiline queries are supported.
@@ -111,13 +109,44 @@ def execute_user_queries(cursor: sqlite3.Cursor) -> NoReturn:
         )
 
 
+def get_query(cursor: sqlite3.Cursor) -> NoReturn:
+    """Generator which gets queries from stdin.
+    Queries are NOT validated at this stage.
+
+    :param cursor: sqlite3 cur object.
+    """
+    print("\nEnter your query below and press Enter twice to execute it:")
+    new_query = ""
+    while True:
+        new_query_line = next(sys.stdin)
+        if new_query_line.isspace():
+            break
+        new_query += new_query_line
+    if not new_query:
+        close_with_message_and_exit(cursor, "No query was detected")
+    yield new_query
+
+
+def close_with_message_and_exit(
+        cursor: sqlite3.Cursor,
+        message="") -> NoReturn:
+    """Prints message, closes the cur and exits
+
+    :param cursor: sqlite3 cur object to be closed
+    :param message: message to print before exit
+    """
+    print(message, end="")
+    cursor.close()
+    exit()
+
+
 def pretty_print_response(
         headers: Iterable[str],
         records: Iterable[Iterable[Union[str, int]]]) -> NoReturn:
     """Prints response as a table. Automatically detects column width.
 
     :param headers: headers from recent selection
-    :param records: response fetched from a cursor
+    :param records: response fetched from a cur
     """
     def _align_fields(column: Iterable[Any]) -> Iterable[str]:
         """Transforms all values in the column
@@ -143,33 +172,8 @@ def pretty_print_response(
 
 
 if __name__ == "__main__":
-    # 0. Set data definition and insertion queries for later use
-    INITIALIZATION_SQL_SCRIPT = """
-    DROP TABLE IF EXISTS projects_tbl;
-    CREATE TABLE projects_tbl(
-        project_id   NUMBER  PRIMARY KEY,
-        name         TEXT,
-        description  TEXT,
-        deadline     DATE
-    );
-    DROP TABLE IF EXISTS tasks_tbl;
-    CREATE TABLE tasks_tbl( 
-        task_id      NUMBER  PRIMARY KEY,
-        priority     INTEGER,
-        details      TEXT,
-        status       TEXT    CHECK(
-            status IN('new', 'pending', 'done', 'canceled')),
-        deadline     DATE,
-        completed    DATE,
-        project_id   NUMBER,
-        FOREIGN KEY(project_id) REFERENCES project_tbl(project_id)
-    );"""
-    # Most names are hardcoded because formatting sql queries
-    # is not safe due to potential SQL injections.
-    # Only ? placeholders are used.
-
     conn = sqlite3.connect("task_db.sqlite3")
     cur = conn.cursor()
-    cur.executescript(INITIALIZATION_SQL_SCRIPT)
+    create_database_schema(cur)
     import_data_into_database(cur)
     execute_user_queries(cur)
